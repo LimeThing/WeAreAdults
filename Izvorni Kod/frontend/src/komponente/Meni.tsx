3c993c51720e8c2105ea81d955b8ca2e6f551a75
Iva Rengel <iva.rengel@fer.hr>
2023-11-04 17:29:24 +0100

    Konacno dobro napravljan router

--- Izvorni Kod/frontend/src/komponente/Meni.tsx
+++ Izvorni Kod/frontend/src/komponente/Meni.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import { MeniMain } from './stilovi'
 
 export default function Meni() {
   return (
-    <MeniMain>Meni</MeniMain>
+    <><MeniMain>Meni</MeniMain><Outlet /></>
   )
 }
