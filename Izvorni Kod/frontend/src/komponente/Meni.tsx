417c3e355a285a694cc7bfdfb11329f6eabcc500
Iva Rengel <iva.rengel@fer.hr>
2023-11-04 17:06:39 +0100

    Dodani styled components i primarni meni

---
+++ Izvorni Kod/frontend/src/komponente/Meni.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MeniMain } from './stilovi'
+
+export default function Meni() {
+  return (
+    <MeniMain>Meni</MeniMain>
+  )
+}
---
+++ Izvorni Kod/frontend/src/komponente/stilovi.ts
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const MeniMain = styled.div`
+    width: 100%;
+    background: #DD4444;
+    display: flex;
+    height: 3rem;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 1rem 0 1rem; 
+`
