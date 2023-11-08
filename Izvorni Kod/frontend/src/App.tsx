108a0eeb1f56d4ac77915eabb70171a2e221d16c
unknown <josipa.sever@fer.hr>
2023-11-08 22:54:04 +0100

    Izrađena stranica za stvaranje akcija

--- Izvorni Kod/frontend/src/App.tsx
+++ Izvorni Kod/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Meni from "./komponente/Meni";
 import Informacije from "./komponente/stranice/Informacije";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StvaranjeAkcija from "./komponente/stranice/StvaranjeAkcija";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Meni />}>
           <Route path="info" element={<Informacije />} />
+          <Route path="stvaranje-akcija" element={<StvaranjeAkcija />} />
         </Route>
       </Routes>
     </BrowserRouter>
