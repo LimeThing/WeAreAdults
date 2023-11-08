2f8b47efd55dc2d5a02bcdaabf865bc9ffe224f0
Iva Rengel <iva.rengel@fer.hr>
2023-11-08 23:04:31 +0100

    Dodani gumbi na meni traku

--- Izvorni Kod/frontend/src/komponente/Meni.tsx
+++ Izvorni Kod/frontend/src/komponente/Meni.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { MeniMain } from './stilovi'
+import { faArrowRightFromBracket, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import {
+  FlexBox,
+  HiddenOnDesktopDiv,
+  HiddenOnMobileDiv,
+  IconButton,
+  MeniButton,
+  TextBox,
+} from "./stilovi";
 
 export default function Meni() {
   return (
-    <><MeniMain>Meni</MeniMain><Outlet /></>
-  )
+    <>
+      <div style={{ background: "#dd4444" }}>
+        <FlexBox $justify="space-between" $height="3rem">
+          <HiddenOnDesktopDiv>
+            <IconButton>
+              <FontAwesomeIcon
+                icon={faBars}
+                size="xl"
+                style={{ color: "#ffffff" }}
+              />
+            </IconButton>
+          </HiddenOnDesktopDiv>
+          <HiddenOnMobileDiv>
+            <FlexBox>
+              <IconButton>
+                <FontAwesomeIcon
+                  icon={faArrowRightFromBracket}
+                  size="xl"
+                  style={{ color: "#ffffff" }}
+                />
+              </IconButton>
+              <TextBox $size="1.2">Ime Prezime</TextBox>
+            </FlexBox>
+          </HiddenOnMobileDiv>
+          <TextBox>DraculaCharity</TextBox>
+          <HiddenOnMobileDiv>
+            <FlexBox>
+              <MeniButton>O nama</MeniButton>
+              <MeniButton>Lokacije</MeniButton>
+              <MeniButton>Registriraj se</MeniButton>
+            </FlexBox>
+          </HiddenOnMobileDiv>
+        </FlexBox>
+      </div>
+      <Outlet />
+    </>
+  );
 }
--- Izvorni Kod/frontend/src/komponente/stilovi.ts
+++ Izvorni Kod/frontend/src/komponente/stilovi.ts
@@ -1,11 +1,59 @@
 import { styled } from "styled-components";
 
-export const MeniMain = styled.div`
-    width: 100%;
-    background: #DD4444;
-    display: flex;
-    height: 3rem;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 1rem 0 1rem; 
-`
+export const FlexBox = styled.div<{ $direction?: string, $height?: string, $width?: string, $align?: string, $justify?: string }>`
+  width: ${(props) => (props.$width ? props.$width : "100%")};
+  display: flex;
+  height: ${(props) => (props.$height ? props.$height : "fit-content")};
+  align-items: ${(props) => (props.$align ? props.$align : "center")};
+  justify-content: ${(props) => (props.$justify ? props.$justify : "center")};
+  flex-direction: ${(props) => (props.$direction ? props.$direction : "row")};
+`;
+
+export const HiddenOnMobileDiv = styled.div`
+  display: inline;
+  @media only screen and (min-width: 768px) {
+    display: "none";
+  }
+`;
+
+export const HiddenOnDesktopDiv = styled.div`
+  display: inline;
+  @media only screen and (max-width: 768px) {
+    display: "none";
+  }
+`;
+
+export const MeniButton = styled.button`
+  width: fit-content;
+  padding: 0 1rem 0 1rem;
+  background: #de5555;
+  height: 2rem;
+  color: white;
+  border: none;
+  border-radius: 2rem;
+  font-size: 0.95rem;
+
+  &:hover {
+    background: #bb3333;
+  }
+`;
+
+export const IconButton = styled.button`
+  width: fit-content;
+  padding: 0.5rem;
+  background: #de5555;
+  height: fit-content;
+  border: none;
+  border-radius: 0.2rem;
+
+  &:hover {
+    background: #bb3333;
+  }
+`;
+
+export const TextBox = styled.div<{ $size?: string }>`
+  width: fit-content;
+  text-align: center;
+  color: white;
+  font-size: ${(props) => (props.$size ? props.$size + "rem" : "1.5rem")};
+`;
