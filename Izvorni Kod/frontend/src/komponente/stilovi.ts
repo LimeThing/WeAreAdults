be66df65c718aa8ae824a444e8450ea5d8aa53db
nikicaIvandic <ni119124624@fer.hr>
2023-12-04 22:55:11 +0100

    Dodana stranica za verifikaciju novoregistriranih korisnika.

--- Izvorni Kod/frontend/src/komponente/stilovi.ts
+++ Izvorni Kod/frontend/src/komponente/stilovi.ts
@@ -262,3 +262,27 @@ export const Drakulica = styled.div`
   height: 200px;
   width: auto;
 `;
+
+export const ListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+export const Name = styled.div`
+  margin-bottom: 8px;
+`;
+
+export const VerifyButton = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
