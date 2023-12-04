5d9a987256c10211eba8f02ebc23ec3feda1843c
Mirta Hnrcic <mirtahrncic@gmail.com>
2023-12-04 21:37:45 +0100

    Dodana registracija korisnika

--- Izvorni Kod/frontend/src/komponente/stranice/Informacije.tsx
+++ Izvorni Kod/frontend/src/komponente/stranice/Informacije.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Informacije() {
   return (
-    <div>Informacije <br/>
-    Nesto nesto nesro <br/>
-    dugacak jako tekst</div>
+    <div>
+      Informacije <br/>
+      Nesto nesto nesro <br/>
+      dugacak jako tekst
+    </div>
   )
 }
