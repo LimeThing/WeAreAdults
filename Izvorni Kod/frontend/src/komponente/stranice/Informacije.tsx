f6ad98554ffa27f57d5628fd40e1d4271fae85b1
Mirta Hnrcic <mirtahrncic@gmail.com>
2023-12-16 22:41:53 +0100

    Sreden css i dodane informacije

--- Izvorni Kod/frontend/src/komponente/stranice/Informacije.tsx
+++ Izvorni Kod/frontend/src/komponente/stranice/Informacije.tsx
@@ -1,11 +1,132 @@
-import React from 'react'
+import React from 'react';
+import { InfoStyle } from "../stilovi";
+import ckNacela from "./ckNacela.jpeg";
+import hck from "./hck.jpeg";
+import darivanjeKrvi from "./darivanjeKrvi.jpg"
+import doniranaKrv from "./doniranaKrv.jpeg"
+
 
 export default function Informacije() {
   return (
-    <div>
-      Informacije <br/>
-      Nesto nesto nesro <br/>
-      dugacak jako tekst
-    </div>
+    <InfoStyle>
+      <br/>
+      <h1>Hrvatski Crveni križ</h1>
+      <br/>
+      <img src={hck} alt="slikaHck" className='hck'></img>
+      <h2>Tko smo?</h2>
+      <br/>
+     <p>Hrvatski Crveni križ nacionalno je društvo Crvenog križa koje nepristrano i bez diskriminacije djeluje na teritoriju Republike Hrvatske, 
+      a u svrhu promicanja i ostvarivanja humanitarnih ciljeva i programa od opće koristi.<br/> <br/>Djeluje kao neprofitna pravna osoba u rješavanju 
+      humanitarnih pitanja, organiziranju zdravstvenih i socijalnih programa, pripremanju stanovništva za djelovanje u masovnim nesrećama i 
+      katastrofama kao i u djelovanju u smanjenju i uklanjanju posljedica masovnih nesreća i katastrofa.<br/> <br/>U sustavu 
+      Hrvatskog Crvenog križa djeluje 131 županijsko, gradsko i općinsko društvo Crvenoga križa, od čega ih je 111 općinskih i gradskih, a 20 županijskih
+      društava Crvenog križa. Gradsko društvo Crvenog križa Zagreb ima i status županijskog društva.<br/> <br/>Općinska, gradska i županijska društva s pravnom 
+      osobnošću imaju sljedeća tijela: skupštinu, predsjednika i potpredsjednike, odbor i nadzorni odbor. U navedena tijela mogu biti birani samo članovi 
+      Hrvatskog Crvenog križa koji funkcije obnašaju volonterski. Svako društvo ima ravnatelja koji funkciju obavlja profesionalno.<br/> <br/>Društva Crvenog križa s pravnom 
+      osobnošću mogu imati ustrojstvene oblike bez pravne osobnosti (gradska, općinska, mjesna, osnovna i područna društva Crvenog križa, aktive, aktive 
+      dobrovoljnih davatelja krvi, klubove, klubove dobrovoljnih davatelja krvi, skupine, sekcije, terenske jedinice i drugo), a što se uređuje statutom društva.</p>
+
+      <br/> 
+      <br/>
+      <img src={ckNacela} alt="slikaNacela" className="nacela"></img>
+      <h3>Temeljna načela</h3>
+      <br/>
+      <p>Kao dio Međunarodnog pokreta Crvenog križa i Crvenog polumjeseca, Hrvatski Crveni križ u svojemu radu vodi se temeljnim načelima Pokreta:</p>
+      <br/>
+      <b>HUMANOST</b>
+      <p>Međunarodni pokret Crvenog križa i Crvenog polumjeseca, nastao u želji da bez diskriminacije pruža pomoć svim ranjenicima na bojnom polju, 
+        nastoji u svim prilikama spriječiti i ublažiti ljudsku patnju. Svrha Pokreta je zaštita života i zdravlja te osiguranje poštivanja ljudske osobe. 
+        On promiče uzajamno razumijevanje, prijateljstvo, suradnju i trajan mir među svim narodima.</p>
+      <br/>
+      <b>NEPRISTRANOST</b>
+      <p>Pokret ne pravi razliku s obzirom na nacionalnu, rasnu, vjersku, klasnu ili političku pripadnost pojedinca, već nastoji ublažiti njegove muke, 
+        isključivo vodeći računa o njegovim potrebama, dajući prednost najhitnijim slučajevima unesrećenih.</p>
+      <br/>
+      <b>NEUTRALNOST</b>
+      <p>Kako bi očuvao povjerenje svih, Pokret se ne opredjeljuje u neprijateljstvima i ne upušta se u rasprave političke, rasne, vjerske 
+        ili ideološke prirode.</p>
+      <br/>
+      <b>NEOVISNOST</b>
+      <p>Pokret je neovisan. Nacionalna društva, iako su pomoćna tijela humanitarnih službi svojih vlada i podvrgnuta zakonima svojih zemalja, moraju 
+        očuvati svoju autonomiju kako bi u svako doba mogla djelovati u skladu s načelima Pokreta.</p>
+      <br/>
+      <b>DOBROVOLJNOST</b>
+      <p>Pokret je dobrovoljan i ne pokreće ga želja za dobitkom.</p>
+      <br/>
+      <b>JEDINSTVO</b>
+      <p>U svakoj zemlji može postojati samo jedno društvo Crvenog križa ili Crvenog polumjeseca. Ono mora biti dostupno svima i provoditi humanitarnu 
+        djelatnost na cjelokupnom teritoriju.</p>
+      <br/>
+      <b>UNIVERZALNOST</b>
+      <p>Međunarodni pokret Crvenog križa i Crvenog polumjeseca, u kojem sva društva imaju jednaki položaj i dijele jednaka prava i dužnosti uzajamnog 
+        pomaganja, svjetski je pokret.</p>
+      
+      <br/>
+      <br/>
+
+      <h2>Što radimo?</h2>
+      <br/>
+      <p>Hrvatski Crveni križ u svojemu radu ostvaruje humanitarne ciljeve i zadaće na području zaštite i unapređenja zdravlja, socijalne skrbi, 
+        zdravstvenog i humanitarnog odgoja te se zalaže za poštovanje međunarodnoga humanitarnog prava i zaštitu ljudskih prava.<br/><br/>
+        Posebne obveze Hrvatski Crveni križ izvršava u situacijama oružanih sukoba, velikih prirodnih, ekoloških, tehnoloških i drugih nesreća i epidemija s
+        posljedicama masovnih stradanja ljudi.<br/><br/>Prva pomoć, zaštita i promicanje zdravlja, rad na bolestima ovisnosti, pripreme i odgovor na krize,
+        spašavanje života na vodi, ekološka zaštita, skrb o tražiteljima međunarodne zaštite, prevencija trgovanja ljudima, briga za socijalno ugrožene građane,
+        služba traženja nestalih osoba te edukacija mladih neki su od temeljnih programa koje HCK provodi s ciljem izgradnje humanijeg, tolerantnijeg i 
+        sigurnijeg društva.</p>
+
+      <br/>
+      <br/>
+
+      <h1>Dobrovoljno darivanje krvi</h1>
+      <img src={darivanjeKrvi} alt="slikaDarivanjeKrvi" className='darivanjeKrvi'></img>
+      <br/>
+      <p>Hrvatski Crveni križ – utemeljitelj dobrovoljnog davanja krvi u Republici Hrvatskoj, promiče dobrovoljno davanje krvi, organizira i provodi akcije davanja 
+        krvi, okuplja davatelje, dodjeljuje priznanja sukladno Pravilniku o priznanjima Hrvatskog Crvenog križa u svrhu društvenog vrednovanja davatelja krvi, vodi 
+        brigu i evidenciju o davateljima krvi  te izdaje potrebne potvrde.<br/><br/>Kako bi se zadovoljile sve potrebe zdravstva Republike Hrvatske za krvi i krvnim 
+        pripravcima, u suradnji s transfuzijskim službama, izrađuje se godišnji Plan prikupljanja krvi i kalendar akcija prema iskazanim potrebama zdravstva Republike 
+        Hrvatske, vodeći brigu da priliv krvi tokom godine dnevno i mjesečno bude kontinuiran i ravnomjeran. Godišnje se u organizaciji društava Hrvatskog Crvenog križa 
+        prikupi oko 160.000 doza krvi, što iznosi 3,5 davanja na 100 stanovnika.<br/><br/>Hrvatski Crveni križ posebnu pozornost posvećuje općoj edukaciji stanovništva u 
+        izgradnji svijesti o važnosti davanja krvi i stvaranju pozitivnog stava o dobrovoljnom davanju krvi kao i kontinuiranoj edukaciji zaposlenika i volontera.<br/><br/>
+        Dan dobrovoljnih davatelja krvi u Republici Hrvatskoj obilježava se 25. listopada. Na taj je dan 1953. u Željezari Sisak provedena prva akcija dobrovoljnog davanja 
+        krvi u organizaciji Hrvatskog Crvenog križa utemeljena na osnovnim načelima dobrovoljnog davanja krvi – <b>daruj krv dobrovoljno, besplatno, solidarno i anonimno</b>. 
+        Takav način prikupljanja krvi smatra se najsigurnijim zbog zdravlja primatelja, a ujedno omogućuje  dostupnost krvi i krvnih pripravaka svima kojima je krv neophodno 
+        potrebna u  liječenju.</p>
+
+        <br/>
+        <br/>
+        <p>U svrhu osiguravanja kvalitetne i sigurne krvi Svjetska zdravstvena organizacija (WHO) i Međunarodna federacija društava Crvenog križa i Crvenog polumjeseca (IFRC), 
+          čiji je član i Hrvatski Crveni križ, usvojile su 1999. dokument kojim se obvezuju da će se osiguranje krvi i krvnih pripravaka temeljiti na dobrovoljnom i neplaćenom 
+          davalaštvu krvi.</p>
+          <br/>
+          <br/>
+          
+          <p>S ciljem podizanja javne svijesti o potrebi za sigurnom krvi i krvnim pripravcima te kao znak zahvalnosti dobrovoljnim davateljima krvi na 
+          njihovom daru života, 14. lipnja proglašen je Svjetskim danom davatelja krvi. Samodostatnost opskrbe zdravstva krvlju i krvnim pripravcima za svaku zemlju je neprocjenjivo 
+          nacionalno bogatstvo, a u određenom trenutku i strateški materijal.</p>
+
+          <br/>
+          <br/>
+          <p>Društva Hrvatskog Crvenog križa redovito organiziraju i provode akcije davanja krvi prema mjestu rada, stanovanja, obrazovanja kao i u transfuzijskim centrima.<br/>
+            Organizacija akcija davanja krvi je proces koji podrazumijeva pripremu za održavanje akcije, usklađeni kalendar akcija s transfuzijskom službom, kontakt s organizatorom,
+            dogovore s volonterima, obavijesti o održavanju akcije, pozivanje davatelja, osiguranje mjesta za zahvalni obrok i zahvalni obrok.<br/><br/>Krv nije moguće proizvesti na 
+            umjetan način stoga osiguravanje dostatnih zaliha krvi ovisi samo o dobrovoljnim davateljima. Kako bi se osiguralo brzo, kvalitetno i sigurno liječenje bolesnika 
+            potrebno je uvijek imati dovoljan broj dobrovoljnih davatelja krvi, a time i dovoljne količine krvi u pričuvi.<br/><br/> Prilikom odlaska na davanje krvi obavezno treba 
+            ponijeti <b>osobnu iskaznicu i iskaznicu davatelja krvi</b> ako je posjedujete. Poželjno je biti odmoren i naspavan.<br/><br/>Hrvatski Crveni križ zahvaljuje svim 
+            dobrovoljnim davateljima krvi na iskazanoj humanosti i plemenitosti te na svakoj darovanoj dozi krvi, jer <b>samo 1 doza može spasiti čak 3 ljudska života</b>.</p>
+            <br/>
+            <br/>
+
+            <img src={doniranaKrv} alt="slikaDoniranaKrv" className='doniranaKrv'></img>
+
+            <br/>
+            <br/>
+
+            <h2>Krv treba čekati bolesnika, a ne obrnuto!</h2>
+    
+ 
+
+
+
+    </InfoStyle>
   )
 }
