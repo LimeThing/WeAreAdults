70d14fe75c5aa72c492c72c80c9a0185afc03a68
unknown <josipa.sever@fer.hr>
2023-12-18 17:09:59 +0100

    Uređena stranica Lokacije

--- Izvorni Kod/frontend/src/komponente/stranice/Lokacije.tsx
+++ Izvorni Kod/frontend/src/komponente/stranice/Lokacije.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { FlexBox, OverFlowDiv, TextBoxLokacije } from "../stilovi";
 import { mockKBC, mockAkcija } from "./komunalije/mock_podatci";
-
+import {
+  faCircleExclamation
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function Lokacije() {
@@ -9,10 +12,10 @@ export default function Lokacije() {
     mockKBC.map((kbc, index) => {
       return <>
       <div style={{width: "90%" }}>
-        <TextBoxLokacije>{kbc.ime}</TextBoxLokacije>
+        <TextBoxLokacije style={{ fontWeight: "bold" }}>{kbc.ime}</TextBoxLokacije>
         <TextBoxLokacije>{kbc.adresa}</TextBoxLokacije>
       </div>
-      {index !== mockKBC.length-1 && <hr style={{border: "solid black", width: "100%"}}/>}
+      {index !== mockKBC.length-1 && <hr style={{width: "100%"}}/>}
       </>
     } )
   
@@ -29,28 +32,34 @@ export default function Lokacije() {
         <TextBoxLokacije>
           {"Kraj: "+akcija.vrijemeKraj.toDateString()}
         </TextBoxLokacije>
-        {akcija.hitna === true && <TextBoxLokacije>{"HITNA AKCIJA! Tip krvi: "+akcija.krGrupa}</TextBoxLokacije>}
+        {akcija.hitna === true && <TextBoxLokacije style={{ fontWeight: "bold"}}><FontAwesomeIcon icon={faCircleExclamation} size="xl"/> {"HITNA AKCIJA! Krvna grupa: "+akcija.krGrupa}</TextBoxLokacije>}
       </div>
-      {index !== mockAkcija.length-1 && <hr style={{border: "solid black", width: "100%"}}/>}
+      {index !== mockAkcija.length-1 && <hr style={{width: "100%"}}/>}
       </>
     })
    
   return (
     <>
       <FlexBox $justify="space-around" $height="100vh">
-
-        <OverFlowDiv $height="400px" $width="40%" style={{border: "solid black 4px"}}>
-          <FlexBox $direction="column"  style={{padding: "1rem 0rem 1rem 0rem", background: "red"}}>
+      
+         <div>
+        <p>Lokacije na kojima uvijek možete darivati krv:</p>
+        <br></br>
+        <OverFlowDiv $height="400px" $width="40%">
+          <FlexBox $direction="column"  style={{padding: "1rem 0rem 1rem 0rem"}}>
             {kbcs}
           </FlexBox>
         </OverFlowDiv>
-
-        <OverFlowDiv $height="400px" $width="40%" style={{border: "solid black 4px"}}>
-          <FlexBox $direction="column"  style={{padding: "1rem 0rem 1rem 0rem", background: "red"}}>
+        </div>
+        <div>
+        <p>Trenutno aktivne akcije:</p>
+        <br></br>
+        <OverFlowDiv $height="400px" $width="40%">
+          <FlexBox $direction="column"  style={{padding: "1rem 0rem 1rem 0rem"}}>
             {akcije}
           </FlexBox>
         </OverFlowDiv>
-
+        </div>
       </FlexBox>
         
     </>
