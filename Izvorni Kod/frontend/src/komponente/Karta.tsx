7f62f1185a95b7c137229458226a820460749e95
unknown <josipa.sever@fer.hr>
2023-12-18 18:18:57 +0100

    Napravljena karta s označenim fiksnim lokacijama

---
+++ Izvorni Kod/frontend/src/komponente/Karta.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Map, Marker } from 'pigeon-maps';
+import { Karta } from './stilovi';
+
+export function MyMap() {
+  const initialCenter: [number, number] = [44.46212, 16.50319];
+  const [center, setCenter] = useState<[number, number]>(initialCenter);
+  const [zoom, setZoom] = useState(7);
+
+  return (
+    <Karta>
+    <Map 
+      height={600} 
+      width={600}
+      zoom={zoom} 
+      center={center}
+      onBoundsChanged={({ center, zoom }) => { 
+        setCenter(center as [number, number]) 
+        setZoom(zoom) 
+      }} 
+    >
+    <Marker anchor={[45.5600001, 18.67588]} color='#b63e3e'/>
+    <Marker anchor={[45.32691, 14.441]} color='#b63e3e'/>
+    <Marker anchor={[45.50695, 16.44238]} color='#b63e3e'/>
+    <Marker anchor={[42.64983, 18.07733]} color='#b63e3e'/>
+    <Marker anchor={[46.30269, 16.32512]} color='#b63e3e'/>
+    <Marker anchor={[44.10752, 15.23459]} color='#b63e3e'/>
+    <Marker anchor={[45.81612, 15.99121]} color='#b63e3e'/>
+    </Map>
+    </Karta>
+  );
+}
+
+export default MyMap;
