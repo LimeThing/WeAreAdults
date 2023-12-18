d164d9e7098293285d584843c60050017e294bf7
Iva Rengel <iva.rengel@fer.hr>
2023-12-18 23:58:29 +0100

    napravljeni endpoints za korisnika

--- Izvorni Kod/backend/main.py
+++ Izvorni Kod/backend/main.py
@@ -1,33 +1,16 @@
-from fastapi import FastAPI, HTTPException, Depends, status
-from pydantic import BaseModel
-from typing import Annotated
+# uvicorn main:app --reload    komanda za pokrenut server
+from fastapi import FastAPI
 import models
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from database import engine
+
+from routers import korisnik
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-
-class kbcBase(BaseModel):
-    ime:str #samo se ime šalje u request, adresa dolazi u odgovori 
-  
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
+# ruteri za svaku klasu
+app.include_router(korisnik.router)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-# uvicorn main:app --reload    komanda za pokrenut server
+# mozete provjerit dal vam rade rute na http://127.0.0.1:8000/docs#/
+# (treba pricekat oko minutu ili restartirati server da se vide promjene)
