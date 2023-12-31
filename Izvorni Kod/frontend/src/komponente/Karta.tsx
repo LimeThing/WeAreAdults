2cdf5964e41ca585763108fcf02a74ed6724ac84
unknown <josipa.sever@fer.hr>
2023-12-31 13:41:28 +0100

    Lokacije - hitne akcije postavljene na vrh, dodane lokacije koje su nedostajale i nazivi na karti

--- Izvorni Kod/frontend/src/komponente/Karta.tsx
+++ Izvorni Kod/frontend/src/komponente/Karta.tsx
@@ -1,38 +1,56 @@
 import React, { useState } from "react";
-import { Map, Marker } from "pigeon-maps";
-import { Karta } from "./stilovi";
+import { Map, Marker, Overlay } from "pigeon-maps";
+import { Karta, LocationsWindow } from "./stilovi";
 import RezervacijaPopup from "./RezervacijaPopup";
+import { mockKBC } from "./stranice/komunalije/mock_podatci";
 
 export function MyMap() {
   const initialCenter: [number, number] = [44.46212, 16.50319];
   const [center, setCenter] = useState<[number, number]>(initialCenter);
-  const [zoom, setZoom] = useState(7);
   const [showPopup, setShowPopup] = useState(false);
+  const [zoom, setZoom] = useState(7.3);
+  const [selectedLocation, setSelectedLocation] = useState<any>(null);
+  
 
   return (
-    <>
-      {showPopup && <RezervacijaPopup closeFun={setShowPopup} close={showPopup} />}
-      <Karta>
-        <Map
-          height={600}
-          width={600}
-          zoom={zoom}
-          center={center}
-          onBoundsChanged={({ center, zoom }) => {
-            setCenter(center as [number, number]);
-            setZoom(zoom);
-          }}
-        >
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[45.5600001, 18.67588]} color="#b63e3e" />
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[45.32691, 14.441]} color="#b63e3e" />
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[45.50695, 16.44238]} color="#b63e3e" />
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[42.64983, 18.07733]} color="#b63e3e" />
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[46.30269, 16.32512]} color="#b63e3e" />
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[44.10752, 15.23459]} color="#b63e3e" />
-          <Marker onClick={() => setShowPopup(!showPopup)} anchor={[45.81612, 15.99121]} color="#b63e3e" />
-        </Map>
-      </Karta>
-    </>
+    <Karta>
+    <Map 
+      height={700} 
+      width={700}
+      zoom={zoom} 
+      center={center}
+      onBoundsChanged={({ center, zoom }) => { 
+        setCenter(center as [number, number]) 
+        setZoom(zoom) 
+      }} 
+      onClick={() => setSelectedLocation(null)}
+    >
+    
+    {mockKBC.map((kbc, index) => (
+          <Marker 
+            key={index} 
+            anchor={[kbc.longitude, kbc.latitude]} 
+            color='#b63e3e' 
+            onClick={() => setSelectedLocation({ ...kbc, index })}
+          />
+        ))}
+
+        {mockKBC.map((kbc, index) => (
+          <Overlay anchor={[kbc.longitude, kbc.latitude - 0.7]} >
+          <LocationsWindow 
+            key={index}
+            style={{
+              display: selectedLocation && selectedLocation.index === index ? 'block' : 'none',
+            }}
+          >
+            <p>{kbc.ime}</p>
+            <button>Rezerviraj termin</button>
+          </LocationsWindow>
+          </Overlay>
+        ))}
+       
+    </Map>
+    </Karta>
   );
 }
 
