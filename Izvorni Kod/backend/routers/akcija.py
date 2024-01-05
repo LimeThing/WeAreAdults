947e82a42bc3c49644857290fb81d5e79848bc85
Zrinka <zrinka.cvitanovic@estudent.hr>
2024-01-05 12:51:07 +0100

    dodani edit endpointovi; tipfeleri u definiciji baze

--- Izvorni Kod/backend/routers/akcija.py
+++ Izvorni Kod/backend/routers/akcija.py
@@ -4,7 +4,8 @@ from pydantic import BaseModel
 import models
 from dependencies import db_dependency
 import enum
-from datetime import datetime
+from datetime import datetime, date
+from typing import Optional
 
 router = APIRouter(
     prefix="/akcija"
@@ -14,16 +15,18 @@ class AkcijaModel(BaseModel):
     idAkcija: int
     imeLokacije: str
     adresa: str
-    datumPoc: datetime
-    datumKraj: datetime
+    datumPoc: date
+    datumKraj: date
     hitna: bool
-    krgrupa: models.KrvnaGrupa
+    #krgrupa: Optional[models.KrvnaGrupa]
     mail: str
 
 # Endpointi za akcije
 @router.post("/create/", status_code=status.HTTP_201_CREATED)
 async def akcija_create(akcija: AkcijaModel, db: db_dependency):
+
     db_akcija = models.Akcija(**akcija.dict())
+    #db_akcija.krgrupa = akcija.krgrupa
     db.add(db_akcija)
     db.commit()
 
@@ -47,8 +50,8 @@ async def akcija_get_hitne(db: db_dependency):
     return akcije
 
 @router.get("/get_aktivne/", status_code=status.HTTP_200_OK)
-async def akcija_get_aktivne(akcija_id: int, db: db_dependency):
-    akcije = db.query(models.Akcija).filter(models.Akcija.datumKraj < date.today()).all()
+async def akcija_get_aktivne(db: db_dependency):
+    akcije = db.query(models.Akcija).filter(models.Akcija.datumKraj >= date.today()).all()
     if akcije is None:
         raise HTTPException(status_code=404, detail='Nema aktivnih akcija')
     return akcije
@@ -64,19 +67,26 @@ async def akcija_delete(akcija_id: int, db: db_dependency):
     #za uređivanje - nisam sigurna radi li, ali za to svakako treba instalirati
     #pip install python-multipart
 
-@router.put("/akcije/{akcija_id}")
-async def update_akcija(akcija_id: int, akcija: AkcijaModel = Form(...)):
-    db=db_dependency
-    trenutna_akcija = await db.akcija_get_one(akcija_id)
+@router.put("/edit/")
+async def update_akcija(id_rez:int, akcija: AkcijaModel, db: db_dependency): 
+    existing_akcija = db.query(models.Akcija).filter(models.Akcija.idAkcija == id_rez).first()
 
-    if trenutna_akcija is None:
+    if existing_akcija is None:
         raise HTTPException(status_code=404, detail="Akcija ne postoji")
     
+    db.delete(existing_akcija)
 
-    trenutna_akcija.imeLokacije = akcija.imeLokacije
-    trenutna_akcija.vrijemePoc = akcija.vrijemePoc
-    trenutna_akcija.mbo = akcija.mbo
-
-    await db.update_rezervacija(trenutna_akcija)
-
-    return trenutna_akcija
+    updated_akcija = models.Akcija(
+        idAkcija=id_rez,
+        imeLokacije=akcija.imeLokacije,
+        adresa=akcija.adresa,
+        datumPoc=akcija.datumPoc,
+        datumKraj=akcija.datumKraj,
+        hitna=akcija.hitna,
+        mail=akcija.mail,
+    )
+    db.add(updated_akcija)
+    db.commit() 
+    #u tijelu odgovora se neće vidjeti nita, ali su promjene puno brže vidljive u bazi nego kod flush()
+    #ako skužimo da stvar bolje radi s flush(), možemo staviti to
+    return updated_akcija
