6b038ab584098b0ba3b0eec965a992a928b8c76b
Iva Rengel <iva.rengel@fer.hr>
2024-01-08 20:42:04 +0100

    dovrsena pretvorba tipova

--- Izvorni Kod/frontend/src/komponente/modeli.ts
+++ Izvorni Kod/frontend/src/komponente/modeli.ts
@@ -1,27 +1,53 @@
-class AkcijaModel(BaseModel):
-    idAkcija: int
-    imeLokacije: str
-    adresa: str
-    datumPoc: date
-    datumKraj: date
-    hitna: bool
-    krgrupa: models.KrvnaGrupa
-    mail: str
+export type AkcijaModel = {
+    idAkcija: number,
+    imeLokacije: string,
+    adresa: string,
+    datumPoc: Date,
+    datumKraj: Date,
+    hitna: boolean,
+    krgrupa: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "0+" | "0-",
+    mail: string
+}
 
-    class KbcModel(BaseModel):
-    id: int
-    ime: str
-    lokacija: str
-    class KorisnikModel(BaseModel):
-    mbo: str
-    oib: str
-    ime: str
-    prezime: str
-    spol: models.Spol
-    dob: int
-    krgrupa: models.KrvnaGrupa
-    mjstan: str
-    favkbc: str
-    verificiran: bool
+export type KBCModel = {
+    naziv: string,
+    adresa: string
+}
+
+export type KorisnikModel = {
+    mbo: string,
+    oib: string,
+    ime: string,
+    prezime: string,
+    spol: "Muško" | "Žensko",
+    dob: number,
+    krgrupa: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "0+" | "0-",
+    mjstan: string,
+    favkbc: string,
+    verificiran: boolean,
+}
+
+    export type LoginInfoModel = {
+    mail: string,
+    lozinka: string,
+    mbo: string,
+}
+
+    export type RezervacijaModel = {
+    idRezervacija: number,
+    vrijemePoc: Date,
+    imeLokacije: string,
+    mbo: string,
+
+}
+
+    export type TerminModel = {
+    idTermin: number,
+    imeLokacije: string,
+    datum: Date,
+    vrijemePoc: Date,
+    vrijemeKraj: Date,
+    zauzeto: boolean,
+}
 
     
