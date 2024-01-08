6414b0096542d4ea8c5a17b4a095670d70bc1436
Robert Klečar <robert.klecar@fer.hr>
2024-01-08 18:54:31 +0100

    izmjene i formatiranje

--- Izvorni Kod/backend/models.py
+++ Izvorni Kod/backend/models.py
@@ -2,12 +2,14 @@ from sqlalchemy import Boolean, Column, Integer, String, Enum, DateTime, Date
 from database import Base
 import enum
 
+
 # Modeli napisani tocno onako kako izgledaju u bazi podataka
 
 # potrebni enumi
 class Spol(enum.Enum):
     MUŠKO = "MUŠKO"
     ŽENSKO = "ŽENSKO"
+
 
 class KrvnaGrupa(enum.Enum):
     Apoz = "Apoz"
@@ -19,9 +21,10 @@ class KrvnaGrupa(enum.Enum):
     Npoz = "Npoz"
     Nneg = "Nneg"
 
+
 class Korisnik(Base):
     __tablename__ = 'korisnik'
-    
+
     mbo = Column(String(100), primary_key=True, index=True)
     oib = Column(String(100), unique=True, nullable=False)
     ime = Column(String(100), nullable=False)
@@ -32,29 +35,33 @@ class Korisnik(Base):
     mjstan = Column(String(100), nullable=False)
     favkbc = Column(String(100), nullable=False)
     verificiran = Column(Boolean, nullable=False)
-    
+
+
 class KBC(Base):
     __tablename__ = 'kbc'
 
     naziv = Column(String(100), primary_key=True, index=True)
     adresa = Column(String(100), nullable=False)
-    
+
+
 class LoginInfo(Base):
     __tablename__ = 'loginInfo'
-    
+
     mail = Column(String(100), primary_key=True, index=True)
     lozinka = Column(String(100), nullable=False)
     mbo = Column(String(50), unique=True, nullable=False)
-    
+
+
 class Administrator(Base):
     __tablename__ = 'administrator'
-    
+
     mail = Column(String(100), primary_key=True, index=True)
     lozinka = Column(String(100), nullable=False)
 
+
 class Termin(Base):
     __tablename__ = 'termin'
-    
+
     idTermin = Column(Integer, primary_key=True, index=True)
     imeLokacije = Column(String(100), nullable=False, index=True)
     datum = Column(DateTime, nullable=False)
@@ -62,17 +69,19 @@ class Termin(Base):
     vrijemeKraj = Column(DateTime, nullable=False)
     zauzeto = Column(Boolean, nullable=False)
 
+
 class Rezervacija(Base):
     __tablename__ = 'rezervacija'
-    
+
     idRezervacija = Column(Integer, primary_key=True, index=True)
     vrijemePoc = Column(DateTime, nullable=False)
     imeLokacije = Column(String(100), nullable=False)
     mbo = Column(String(9), unique=True, nullable=False, index=True)
 
+
 class Akcija(Base):
     __tablename__ = 'akcija'
-    
+
     idAkcija = Column(Integer, primary_key=True, index=True)
     imeLokacije = Column(String(100), nullable=False, index=True)
     adresa = Column(String(100), nullable=False, index=True)
