2e0299c6f85992bd15982266f1e8dbde1847ccea
Iva Rengel <iva.rengel@fer.hr>
2024-01-08 23:40:01 +0100

    Fetchanje login informacija

--- Izvorni Kod/frontend/src/komponente/stranice/Login.tsx
+++ Izvorni Kod/frontend/src/komponente/stranice/Login.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { useNavigate } from 'react-router-dom';
-import { Container, FormContainer2, OuterContainer, Toggle2, ToggleContainer2, TogglePanel } from "../stilovi";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import {
+  Container,
+  FormContainer2,
+  OuterContainer,
+  Toggle2,
+  ToggleContainer2,
+  TogglePanel,
+} from "../stilovi";
+import { useGetLoginInfo } from "./udice/useGetLoginInfo";
 
 interface FormData {
   email: string;
@@ -17,53 +25,64 @@ export default function Login() {
   });
 
   const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
 
   const handleRegisterButtonClick = () => {
-    navigate('/Registracija');
+    navigate("/Registracija");
   };
 
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     resolver: yupResolver(schema),
   });
 
+  const {data} = useGetLoginInfo(email);
+
   const onSubmit = (data: FormData) => {
     console.log(data);
+    setEmail(data.email);
   };
 
   return (
-    <OuterContainer>
-    <Container>
-      <FormContainer2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h1>Dobro došli!</h1>
-          <br></br>
-          <span>Unesite svoj email i lozinku</span>
-          <br></br>
-          
-          <input type="text" placeholder="Email..." {...register("email")} />
-          <p> {errors.email?.message} </p>
-          <input
-            type="password"
-            placeholder="Password..."
-            {...register("password")}
-          />
-          <p> {errors.password?.message} </p>
-          <button type="submit">Prijavi se</button>
-        </form>
-      </ FormContainer2>
+    <><h1>{data?.mbo}</h1><OuterContainer>
+      <>
+        <Container>
+          <FormContainer2>
 
-      <ToggleContainer2>
-        <Toggle2>
-          <TogglePanel $right>
-            <h1>Prvi put?</h1>
-            <p>Unesite svoje podatke i napravite korisnički račun</p>
-            <button id="register" onClick={handleRegisterButtonClick}>
-              Registriraj se
-            </button>
-          </ TogglePanel>
-        </ Toggle2>
-      </ ToggleContainer2>
-    </ Container>
-    </ OuterContainer>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <h1>Dobro došli!</h1>
+              <br></br>
+              <span>Unesite svoj email i lozinku</span>
+              <br></br>
+
+              <input type="text" placeholder="Email..." {...register("email")} />
+              <p> {errors.email?.message} </p>
+              <input
+                type="password"
+                placeholder="Password..."
+                {...register("password")} />
+              <p> {errors.password?.message} </p>
+              <button type="submit">Prijavi se</button>
+            </form>
+          </FormContainer2>
+
+          <ToggleContainer2>
+            <Toggle2>
+              <TogglePanel $right>
+                <h1>Prvi put?</h1>
+                <p>Unesite svoje podatke i napravite korisnički račun</p>
+                <button id="register" onClick={handleRegisterButtonClick}>
+                  Registriraj se
+                </button>
+              </TogglePanel>
+            </Toggle2>
+          </ToggleContainer2>
+        </Container>
+      </>
+    </OuterContainer></>
   );
-};
+}
