8dd416952276bdb823c10ad14ef8c22fc3277df2
Robert Klečar <robert.klecar@fer.hr>
2024-01-09 01:24:11 +0100

    dropdown i dropdownmeni

---
+++ Izvorni Kod/frontend/src/komponente/DropDown.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+type DropDownProps = {
+  elementi: string[];
+  showDropDown: boolean;
+  toggleDropDown: Function;
+  biranjeElemenata: Function;
+};
+
+const DropDown: React.FC<DropDownProps> = ({
+    elementi,
+  biranjeElemenata,
+}: DropDownProps): JSX.Element => {
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
+
+
+  const onClickHandler = (elem: string): void => {
+    biranjeElemenata(elem);
+  };
+
+  useEffect(() => {
+    setShowDropDown(showDropDown);
+  }, [showDropDown]);
+
+  return (
+    <>
+      <div className={showDropDown ? 'dropdown' : 'dropdown active'}>
+        {elementi.map(
+          (elem: string, index: number): JSX.Element => {
+            return (
+              <p
+                key={index}
+                onClick={(): void => {
+                  onClickHandler(elem);
+                }}
+              >
+                {elem}
+              </p>
+            );
+          }
+        )}
+      </div>
+    </>
+  );
+};
+
+export default DropDown;
