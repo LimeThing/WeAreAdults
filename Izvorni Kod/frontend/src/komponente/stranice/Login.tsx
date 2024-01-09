fd13e42bd6dbb26dca2963a718efa8f21b0dbbe7
Iva Rengel <iva.rengel@fer.hr>
2024-01-09 01:28:33 +0100

    prijava dodana uz refresh

--- Izvorni Kod/frontend/src/komponente/stranice/Login.tsx
+++ Izvorni Kod/frontend/src/komponente/stranice/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -12,6 +12,8 @@ import {
   TogglePanel,
 } from "../stilovi";
 import { useGetLoginInfo } from "./udice/useGetLoginInfo";
+import useCookies from "./udice/useCookies";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface FormData {
   email: string;
@@ -39,50 +41,58 @@ export default function Login() {
     resolver: yupResolver(schema),
   });
 
-  const {data} = useGetLoginInfo(email);
+  const { data } = useGetLoginInfo(email);
+  const queryClient = useQueryClient()
+  const { setToken } = useCookies();
 
   const onSubmit = (data: FormData) => {
     console.log(data);
     setEmail(data.email);
   };
 
+  useEffect(() => {
+    setToken(data ? data.mbo : "")
+    console.log("set to " + data?.mbo)
+    queryClient.invalidateQueries({ queryKey: ["getKorisnikIme"]})
+    if (data !== undefined) {
+      navigate("/lokacije");
+    }
+  }, [data, navigate, queryClient, setToken]);
+
   return (
-    <><h1>{data?.mbo}</h1><OuterContainer>
-      <>
-        <Container>
-          <FormContainer2>
+    <OuterContainer>
+      <Container>
+        <FormContainer2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Dobro došli!</h1>
+            <br></br>
+            <span>Unesite svoj email i lozinku</span>
+            <br></br>
 
+            <input type="text" placeholder="Email..." {...register("email")} />
+            <p> {errors.email?.message} </p>
+            <input
+              type="password"
+              placeholder="Password..."
+              {...register("password")}
+            />
+            <p> {errors.password?.message} </p>
+            <button type="submit">Prijavi se</button>
+          </form>
+        </FormContainer2>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <h1>Dobro došli!</h1>
-              <br></br>
-              <span>Unesite svoj email i lozinku</span>
-              <br></br>
-
-              <input type="text" placeholder="Email..." {...register("email")} />
-              <p> {errors.email?.message} </p>
-              <input
-                type="password"
-                placeholder="Password..."
-                {...register("password")} />
-              <p> {errors.password?.message} </p>
-              <button type="submit">Prijavi se</button>
-            </form>
-          </FormContainer2>
-
-          <ToggleContainer2>
-            <Toggle2>
-              <TogglePanel $right>
-                <h1>Prvi put?</h1>
-                <p>Unesite svoje podatke i napravite korisnički račun</p>
-                <button id="register" onClick={handleRegisterButtonClick}>
-                  Registriraj se
-                </button>
-              </TogglePanel>
-            </Toggle2>
-          </ToggleContainer2>
-        </Container>
-      </>
-    </OuterContainer></>
+        <ToggleContainer2>
+          <Toggle2>
+            <TogglePanel $right>
+              <h1>Prvi put?</h1>
+              <p>Unesite svoje podatke i napravite korisnički račun</p>
+              <button id="register" onClick={handleRegisterButtonClick}>
+                Registriraj se
+              </button>
+            </TogglePanel>
+          </Toggle2>
+        </ToggleContainer2>
+      </Container>
+    </OuterContainer>
   );
 }
---
+++ Izvorni Kod/frontend/src/komponente/stranice/udice/useGetKorisnik.tsx
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query"
+import { api } from "../../../api"
+
+const getKorisnikIme = (mbo: string): Promise<string> => {
+return api.get("/korisnik/get_names_one/" + mbo).then((response: any) => response.data);
+
+}
+
+export const useGetKorisnikIme = (mbo:string) => {
+    return useQuery<string>({
+        queryKey: ["getKorisnikIme", mbo],
+        queryFn: () => getKorisnikIme(mbo),
+        })
+}
