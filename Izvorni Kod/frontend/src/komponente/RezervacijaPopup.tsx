ed0ba24f90e65e8dab6ae000f94e51a848af8841
Iva Rengel <iva.rengel@fer.hr>
2024-01-18 16:55:59 +0100

    popravljena prijava i rezervacija termina

--- Izvorni Kod/frontend/src/komponente/RezervacijaPopup.tsx
+++ Izvorni Kod/frontend/src/komponente/RezervacijaPopup.tsx
@@ -3,22 +3,25 @@ import React, { useEffect, useState } from "react";
 import { api } from "../api";
 import DropDownMeni from "./DropDownMeni";
 import { RezervacijaModel, TerminModel } from "./modeli";
-import { PopupContainer} from "./stilovi";
+import { FlexBox, PopupContainer } from "./stilovi";
 import useToken from "./stranice/udice/useCookies";
+import formatDate from "./stranice/udice/useFormatDate";
 
 type RezervacijaPopupProps = {
-  closeFun: React.Dispatch<React.SetStateAction<boolean>>,
-  close: boolean,
-  imeLokacije?: string,
+  closeFun: React.Dispatch<React.SetStateAction<boolean>>;
+  close: boolean;
+  imeLokacije?: string;
 };
 
-export default function RezervacijaPopup({ closeFun, close, imeLokacije }: RezervacijaPopupProps) {
-
+export default function RezervacijaPopup({
+  closeFun,
+  close,
+  imeLokacije,
+}: RezervacijaPopupProps) {
   const fetchTermini = (): Promise<TerminModel[]> =>
     api
       .get("/termin/get_by_location/" + imeLokacije)
       .then((response: any) => response.data);
-
 
   const { isSuccess, data: termini } = useQuery<TerminModel[]>({
     queryKey: ["getTermini", imeLokacije],
@@ -26,7 +29,7 @@ export default function RezervacijaPopup({ closeFun, close, imeLokacije }: Rezer
     enabled: !!imeLokacije,
   });
 
-  const {token} = useToken();
+  const { token } = useToken();
 
   const { mutate: postRezervacija } = useMutation({
     mutationFn: (rezervacija: RezervacijaModel) => {
@@ -34,33 +37,67 @@ export default function RezervacijaPopup({ closeFun, close, imeLokacije }: Rezer
     },
   });
 
-  const [terminLista, setTerminLista] = useState<string[]>([])
-  const [selectedTermin, setSelectedTermin] = useState<Date>(new Date())
+  const [terminLista, setTerminLista] = useState<string[]>([]);
+  const [selectedTermin, setSelectedTermin] = useState<Date>(new Date());
 
   useEffect(() => {
     var help: string[] = [];
-    termini?.map((termin) => {if (!termin.zauzeto) {
-      let terminnew = new Date(termin.vrijemePoc)
-      help.push(terminnew.toUTCString())
-    }
-    })
-    setTerminLista(help)
+    termini?.map((termin) => {
+      if (!termin.zauzeto) {
+        let terminnew = new Date(termin.vrijemePoc);
+        help.push(
+          `${
+            terminnew.getHours() <= 9
+              ? "0" + terminnew.getHours()
+              : terminnew.getHours()
+          }:${
+            terminnew.getMinutes() <= 9
+              ? "0" + terminnew.getMinutes()
+              : terminnew.getMinutes()
+          }`
+        );
+      }
+    });
+    setTerminLista(help);
   }, [isSuccess]);
 
   return (
-    <PopupContainer>
+    <PopupContainer style={{ padding: "1rem", borderRadius: "20px" }}>
       <div>Rezervacija termina</div>
-      <DropDownMeni elementi = {terminLista} setSelected={setSelectedTermin}/>
-      <button onClick={() => {
-        let rezervacija: RezervacijaModel = {
-          idRezervacija: 0,
-          vrijemePoc: selectedTermin,
-          imeLokacije: imeLokacije ?? "",
-          mbo: token ?? "111111111"
-        };
-         postRezervacija(rezervacija);
-      }}>Rezerviraj</button>
-      <button onClick={() => closeFun(!close)}>Zatvori</button>
+      <DropDownMeni elementi={terminLista} setSelected={setSelectedTermin} />
+      <FlexBox>
+        <button
+          onClick={() => {
+            let today = new Date();
+            today.setDate(today.getDate() + 1);
+            let rezervacija: RezervacijaModel = {
+              idRezervacija: 0,
+              vrijemePoc: new Date(
+                `${formatDate(today)}T${
+                  selectedTermin.getHours() <= 9
+                    ? "0" + selectedTermin.getHours()
+                    : selectedTermin.getHours()
+                }:${
+                  selectedTermin.getMinutes() <= 9
+                    ? "0" + selectedTermin.getMinutes()
+                    : selectedTermin.getMinutes()
+                }:${
+                  selectedTermin.getMinutes() <= 9
+                    ? "0" + selectedTermin.getMinutes()
+                    : selectedTermin.getMinutes()
+                }.937Z`
+              ),
+              imeLokacije: imeLokacije ?? "",
+              mbo: token ?? "111111111",
+            };
+            postRezervacija(rezervacija);
+            closeFun(!close)
+          }}
+        >
+          Rezerviraj
+        </button>
+        <button onClick={() => closeFun(!close)}>Zatvori</button>
+      </FlexBox>
     </PopupContainer>
   );
 }
