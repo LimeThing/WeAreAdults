7c371005bb3603c77071dae8622ede540f43da3e
unknown <josipa.sever@fer.hr>
2024-01-18 21:12:36 +0100

    Označene hitne akcije s korisnikovom krvnom grupom

--- Izvorni Kod/frontend/src/komponente/stranice/Lokacije.tsx
+++ Izvorni Kod/frontend/src/komponente/stranice/Lokacije.tsx
@@ -5,7 +5,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MyMap from "../Karta";
-import { AkcijaModel, KBCModel } from "../modeli";
+import { AkcijaModel, KBCModel, KorisnikModel } from "../modeli";
 import { api } from "../../api";
 import { useQuery } from "@tanstack/react-query";
 
@@ -13,6 +13,9 @@ export default function Lokacije() {
 
   const [aktivneAkcije, setAktivneAkcije] = useState<AkcijaModel[]>([])
   const [KBC, setKBC] = useState<KBCModel[]>([])
+  const [korisnik, setKorisnik] = useState<KorisnikModel>()
+  const [mbo, setMbo] = useState(sessionStorage.getItem('mbo'));
+  console.log(mbo);
 
   const fetchAkcije = (): Promise<AkcijaModel[]> =>
     api
@@ -24,6 +27,10 @@ export default function Lokacije() {
     .get("/kbc/get_all")
     .then((response: any) => response.data);
 
+  const fetchKorisnik = ():Promise<KorisnikModel> =>
+    api.get(`/korisnik/get_one/${mbo}`)
+    .then((Response: any) => Response.data);
+
   const { isLoading: isLoadingAktivne, data: dataAktivne } = useQuery<AkcijaModel[]>({
     queryKey: ["getAkcija"],
     queryFn: fetchAkcije,
@@ -34,10 +41,16 @@ export default function Lokacije() {
     queryFn: fetchKbc,
   });
 
+  const { isLoading: isLoadingKorisnik, data: dataKorisnik } = useQuery<KorisnikModel>({
+    queryKey: ["getKorisnik"],
+    queryFn: fetchKorisnik,
+  })
+
   useEffect(() => {
     setAktivneAkcije(dataAktivne ? dataAktivne : []);
     setKBC(dataKbc ? dataKbc : []);
-  }, [dataAktivne, dataKbc]);
+    setKorisnik(dataKorisnik);
+  }, [dataAktivne, dataKbc, dataKorisnik]);
 
   const kbcs = 
     KBC.map((kbc, index) => {
@@ -51,8 +64,15 @@ export default function Lokacije() {
     });
   
     const hitneAkcije = aktivneAkcije.filter((akcija) => akcija.hitna).map((akcija, index) => (
-        <div style={{ width: "100%" }} key={index}>
-          <p style={{ fontWeight: "bold", color: "#a82d2d" }}>
+        <div style={{ 
+          width: "100%", 
+          background: akcija.krgrupa === korisnik?.krgrupa && akcija.imeLokacije === korisnik?.favkbc ? "#a82d2d" : "white", 
+          border: akcija.krgrupa === korisnik?.krgrupa ? "#a82d2d solid 10px" : "none",
+          color: akcija.krgrupa === korisnik?.krgrupa && akcija.imeLokacije === korisnik?.favkbc ? "white" : "black",
+          borderRadius: index === 0 ? "30px 30px 0 0" : "0"
+          }} 
+          key={index}>
+          <p style={{ fontWeight: "bold", color: akcija.krgrupa === korisnik?.krgrupa && akcija.imeLokacije === korisnik?.favkbc ? "white" : "#a82d2d", padding: "1rem 0 0 0" }}>
             <FontAwesomeIcon icon={faCircleExclamation} size="xl" /> {"HITNA AKCIJA! Krvna grupa: " + akcija.krgrupa}
           </p>
           <p style={{ fontWeight: "bold"}}>
@@ -75,16 +95,12 @@ export default function Lokacije() {
     const akcije = aktivneAkcije
     .filter((actions) => !actions.hitna)
     .map((actions, index) => (
-      <div style={{ width: "100%" }} key={index}>
+      <div style={{ width: "100%", padding:"1rem 0 0rem 0",
+      borderRadius: index === 0 ? "30px 30px 0 0" : "0"}} key={index}>
         <p style={{ fontWeight: "bold" }}>{actions.imeLokacije}</p>
         <p>{actions.adresa}</p>
         <p>{"Početak: " + actions.datumPoc}</p>
         <p style={{padding: "0 0 1rem 0"}}>{"Kraj: " + actions.datumKraj}</p>
-        {actions.hitna && (
-          <p className="hitna">
-            <FontAwesomeIcon icon={faCircleExclamation} size="xl" color="red"/> {"HITNA AKCIJA! Krvna grupa: " + actions.krgrupa}
-          </p>
-        )}
         {index !== aktivneAkcije.length - hitneAkcije.length - 1 && <hr style={{ width: "100%" }} />}
       </div>
     ));
@@ -107,7 +123,7 @@ export default function Lokacije() {
         <div>
           <h2>Trenutno aktivne akcije:</h2>
           <OverFlowDiv $height="fit-content" $width="fit-content">
-            <FlexBox $direction="column" style={{padding: "1rem 0rem 0rem 0rem"}}>
+            <FlexBox $direction="column" style={{gap: "0", borderRadius: "30px"}}>
               {hitneAkcije}
               {akcije}
             </FlexBox>
