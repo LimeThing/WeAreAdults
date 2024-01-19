1adca86e68d60344f25b501e209a3f286aefec5a
Iva Rengel <iva.rengel@fer.hr>
2024-01-19 11:53:26 +0100

    deploy 2

--- Izvorni Kod/frontend/src/api.tsx
+++ Izvorni Kod/frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://we-are-adults-backend-git-deploy-ivar998-gmailcom.vercel.app'
+    baseURL: 'https://we-are-adults-backend.vercel.app'
 })
